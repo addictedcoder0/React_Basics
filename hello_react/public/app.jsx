3856fa28a247629ca049a2bccc953798f97e0a35
abhishek <abhishek-2863@abhishek-2863.csez.zohocorpin.com>
2017-04-07 11:13:18 +0530

    jsx + component defined + props + defaultProps

--- hello_react/public/app.jsx
+++ hello_react/public/app.jsx
@@ -1,4 +1,30 @@
+var Greeting =  React.createClass({
+    getDefaultProps : function(){
+      return {
+        name: 'React' ,
+        message : 'Message from Default section '
+      };
+    },
+
+    render :function(){
+      var name = this.props.name;
+      var message = this.props.message;
+      return (
+        <div>
+          <h1>Hello {name}</h1>
+          <h3>{message +' !!!'}</h3>
+        </div>
+      )
+    }
+});
+
+var author_name = 'Rudra'
+// rendering with property
 ReactDOM.render(
-  <h1>Hello React</h1>,
-  document.getElementById('app')
+  <Greeting name={author_name} message="Message passed as an attribute "/>,document.getElementById('app')
 );
+
+//rendering without property
+// ReactDOM.render(
+//   <Greeting />,document.getElementById('app')
+// );
